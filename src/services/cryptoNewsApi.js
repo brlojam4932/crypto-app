be09270f72cde223621f69303ef235ade827f37a
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-11-19 19:35:46 -0800

    newsApi fixed
    fixed: deleted headers from object, cleaned up code

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
 const cryptoNewsHeaders = {
-  headers: {
+ 
     'x-bingapis-sdk': 'true',
     'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
     'x-rapidapi-key': '9271cb1bffmsh3bfde2fc26f9dd1p125f3cjsn6324533a44df'
-  }
+  
 };
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
@@ -18,10 +18,9 @@ export const cryptoNewsApi = createApi({
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
       query: ({newsCategory, count}) => createRequest(
-        `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
-        ),
-    })
-  })
+        `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
 });
 
 export const { useGetCryptoNewsQuery } = cryptoNewsApi;
