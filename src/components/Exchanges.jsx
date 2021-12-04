9f7cd155e0756a3391cf4a306f3f41ba9a87ba98
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-12-03 19:45:20 -0800

    app css
    updated css page to original styling

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -20,7 +20,9 @@ function Exchanges() {
     <>
       <Row>
         <Col span={6}>Exchanges</Col>
-
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
       </Row>
 
       <Row>
